27e364d47688c8ba93fb0cac05bec3e93ff560b6
Neil Viloria <neilcviloria@gmail.com>
2024-12-01 20:16:11 -0700

    sort of renders

--- raycast-game/src/lib.rs
+++ raycast-game/src/lib.rs
@@ -1,65 +1,59 @@
 mod utils;
 
+use std::cell::RefCell;
+use std::f64::consts::PI;
+use std::rc::Rc;
 use wasm_bindgen::prelude::*;
-use web_sys::{console, js_sys::Math::sqrt};
+use web_sys::console;
+use web_sys::KeyboardEvent;
 
 static MAP: &'static [[char; 8]; 8] = &[
     ['w', 'w', 'w', 'w', 'w', 'w', 'w', 'w'],
-    ['w', '0', '1', '0', '0', '0', '0', 'w'],
-    ['w', '0', '1', '0', '0', '3', '0', 'w'],
-    ['w', '0', '1', '0', '0', '0', '0', 'w'],
-    ['w', '0', '0', '0', '2', '2', '0', 'w'],
-    ['w', '0', '0', '0', '0', '2', '0', 'w'],
-    ['w', '0', '0', '0', '0', '2', '0', 'w'],
+    ['w', '0', '0', '0', '0', '0', '0', 'w'],
+    ['w', '0', '0', '0', '0', '0', '0', '1'],
+    ['w', '0', '0', '0', '0', '0', '0', 'w'],
+    ['w', '0', '0', '0', '0', '0', '0', '2'],
+    ['w', '0', '0', '0', '0', '0', '0', 'w'],
+    ['w', '0', '0', '0', '0', '0', '0', '3'],
     ['w', 'w', 'w', 'w', 'w', 'w', 'w', 'w'],
 ];
 
 static MAP_GRID_CELL_SIZE_PX: f64 = 30.0;
 
-static FOV: f64 = 100.0;
-static CANVAS_WIDTH: f64 = 600.0;
-static RAYS: f64 = 100.0;
-static DEG_RAY: f64 = FOV / RAYS;
-static PX_RAY: f64 = CANVAS_WIDTH / RAYS;
-
-static SCREEN_WIDTH: u8 = 64;
-
-struct Vec2D {
-    x: f64,
-    y: f64,
+#[derive(Debug)]
+struct Vec2D<T> {
+    x: T,
+    y: T,
 }
 
+#[derive(Debug)]
 struct Player {
-    pos_x: f64,
-    pos_y: f64,
-
-    dir_x: f64,
-    dir_y: f64,
-
-    camera_x: f64,
-    camera_y: f64,
+    position: Vec2D<f64>,
+    direction: Vec2D<f64>,
+    camera: Vec2D<f64>,
 }
 
-enum Side {
+enum Axis {
     Vertical,
     Horizontal,
 }
 
-pub fn cast(x: f64, y: f64, heading: f64) {}
+static STEP: f64 = 0.1;
+
+static ROTATION_SPEED: f64 = 10.0 * (PI / 180.0);
 
 #[wasm_bindgen(start)]
 pub fn main() {
     utils::set_panic_hook();
 
-    let document = web_sys::window().unwrap().document().unwrap();
+    let window = Rc::new(RefCell::new(web_sys::window().unwrap()));
+    let document = window.borrow().document().unwrap();
 
     let game_canvas_element = document.get_element_by_id("game-viewport").unwrap();
-
     let game_canvas: web_sys::HtmlCanvasElement = game_canvas_element
         .dyn_into::<web_sys::HtmlCanvasElement>()
         .map_err(|_| ())
         .unwrap();
-
     let game_canvas_2d_context = game_canvas
         .get_context("2d")
         .unwrap()
@@ -67,7 +61,6 @@ pub fn main() {
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
 
-    // ### map ###
     let map_canvas_element = document.get_element_by_id("map").unwrap();
     let map_canvas: web_sys::HtmlCanvasElement = map_canvas_element
         .dyn_into::<web_sys::HtmlCanvasElement>()
@@ -80,187 +73,289 @@ pub fn main() {
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
 
-    for (row_index, row) in MAP.iter().enumerate() {
-        for (col_index, cell) in row.iter().enumerate() {
-            let fill_style = match *cell {
-                'w' => "black",
+    // for (row_index, row) in MAP.iter().enumerate() {
+    //     for (col_index, cell) in row.iter().enumerate() {
+    //         let fill_style = match *cell {
+    //             'w' => "black",
+    //             '1' => "red",
+    //             '2' => "green",
+    //             '3' => "blue",
+    //             _ => "white",
+    //         };
+    //
+    //         map_canvas_2d_context.set_fill_style_str(fill_style);
+    //
+    //         let x = col_index as f64 * MAP_GRID_CELL_SIZE_PX;
+    //         let y = row_index as f64 * MAP_GRID_CELL_SIZE_PX;
+    //         map_canvas_2d_context.fill_rect(x, y, MAP_GRID_CELL_SIZE_PX, MAP_GRID_CELL_SIZE_PX);
+    //     }
+    // }
+
+    // ######
+    let player = Rc::new(RefCell::new(Player {
+        position: Vec2D { x: 2.0, y: 2.0 },
+        direction: Vec2D { x: 1.0, y: 0.0 },
+        camera: Vec2D { x: 0.0, y: 0.66 },
+    }));
+    let player_clone = player.clone();
+
+    let keydown_handler = Closure::<dyn FnMut(KeyboardEvent)>::new(move |event: KeyboardEvent| {
+        let mut player = player_clone.borrow_mut(); // Access player with interior mutability
+        match event.key().as_str() {
+            // Move forward
+            "w" => {
+                player.position.x += player.direction.x * STEP;
+                player.position.y += player.direction.y * STEP;
+            }
+            // Move backward
+            "s" => {
+                player.position.x -= player.direction.x * STEP;
+                player.position.y -= player.direction.y * STEP;
+            }
+            // Strafe left
+            // "a" => {
+            //     player.position.x -= player.direction.y * STEP;
+            //     player.position.y -= player.direction.x * STEP;
+            // }
+            // // Strafe right
+            // "d" => {
+            //     player.position.x += player.direction.y * STEP;
+            //     player.position.y += player.direction.x * STEP;
+            // }
+            // Rotate clockwise (right)
+            "ArrowRight" => {
+                // Rotate direction
+                let new_dir_x = player.direction.x * f64::cos(ROTATION_SPEED)
+                    - player.direction.y * f64::sin(ROTATION_SPEED);
+                let new_dir_y = player.direction.x * f64::sin(ROTATION_SPEED)
+                    + player.direction.y * f64::cos(ROTATION_SPEED);
+                player.direction.x = new_dir_x;
+                player.direction.y = new_dir_y;
+
+                // Rotate camera
+                let new_camera_x = player.camera.x * f64::cos(ROTATION_SPEED)
+                    - player.camera.y * f64::sin(ROTATION_SPEED);
+                let new_camera_y = player.camera.x * f64::sin(ROTATION_SPEED)
+                    + player.camera.y * f64::cos(ROTATION_SPEED);
+                player.camera.x = new_camera_x;
+                player.camera.y = new_camera_y;
+            }
+            "ArrowLeft" => {
+                // Rotate direction
+                let new_dir_x = player.direction.x * f64::cos(-ROTATION_SPEED)
+                    - player.direction.y * f64::sin(-ROTATION_SPEED);
+                let new_dir_y = player.direction.x * f64::sin(-ROTATION_SPEED)
+                    + player.direction.y * f64::cos(-ROTATION_SPEED);
+                player.direction.x = new_dir_x;
+                player.direction.y = new_dir_y;
+
+                // Rotate camera
+                let new_camera_x = player.camera.x * f64::cos(-ROTATION_SPEED)
+                    - player.camera.y * f64::sin(-ROTATION_SPEED);
+                let new_camera_y = player.camera.x * f64::sin(-ROTATION_SPEED)
+                    + player.camera.y * f64::cos(-ROTATION_SPEED);
+                player.camera.x = new_camera_x;
+                player.camera.y = new_camera_y;
+            }
+
+            // No-op for other keys
+            _ => {}
+        }
+
+        console::log_3(
+            &"position".into(),
+            &player.position.x.into(),
+            &player.position.y.into(),
+        );
+        console::log_3(
+            &"direaction".into(),
+            &player.direction.x.into(),
+            &player.direction.y.into(),
+        );
+
+        console::log_3(
+            &"camera".into(),
+            &player.camera.x.into(),
+            &player.camera.y.into(),
+        );
+    });
+    let _ = window
+        .borrow()
+        .add_event_listener_with_callback("keydown", keydown_handler.as_ref().unchecked_ref());
+
+    keydown_handler.forget();
+
+    let player_clone = player.clone();
+    let map_canvas_2d_context_clone = map_canvas_2d_context.clone();
+
+    let f: Rc<RefCell<Option<Closure<dyn Fn()>>>> = Rc::new(RefCell::new(None));
+    let g = f.clone();
+
+    let window_clone = window.clone();
+
+    // animation loop
+    *g.borrow_mut() = Some(Closure::new(move || {
+        let player = player_clone.borrow();
+
+        // Clear the map canvas
+        map_canvas_2d_context_clone.clear_rect(
+            0.0,
+            0.0,
+            map_canvas.width() as f64,
+            map_canvas.height() as f64,
+        );
+
+        game_canvas_2d_context.clear_rect(
+            0.0,
+            0.0,
+            game_canvas.width() as f64,
+            game_canvas.height() as f64,
+        );
+
+        // Redraw the map grid
+        for (row_index, row) in MAP.iter().enumerate() {
+            for (col_index, cell) in row.iter().enumerate() {
+                let fill_style = match *cell {
+                    'w' => "black",
+                    '1' => "red",
+                    '2' => "green",
+                    '3' => "blue",
+                    _ => "white",
+                };
+
+                map_canvas_2d_context_clone.set_fill_style_str(fill_style);
+
+                let x = col_index as f64 * MAP_GRID_CELL_SIZE_PX;
+                let y = row_index as f64 * MAP_GRID_CELL_SIZE_PX;
+                map_canvas_2d_context_clone.fill_rect(
+                    x,
+                    y,
+                    MAP_GRID_CELL_SIZE_PX,
+                    MAP_GRID_CELL_SIZE_PX,
+                );
+            }
+        }
+
+        map_canvas_2d_context_clone.begin_path();
+        map_canvas_2d_context_clone.move_to(
+            player.position.x * MAP_GRID_CELL_SIZE_PX,
+            player.position.y * MAP_GRID_CELL_SIZE_PX,
+        );
+        map_canvas_2d_context_clone.line_to(
+            (player.position.x * MAP_GRID_CELL_SIZE_PX)
+                + (player.direction.x * MAP_GRID_CELL_SIZE_PX),
+            (player.position.y * MAP_GRID_CELL_SIZE_PX)
+                + (player.direction.y * MAP_GRID_CELL_SIZE_PX),
+        );
+        map_canvas_2d_context_clone.stroke();
+
+        map_canvas_2d_context_clone.set_fill_style_str("red");
+        map_canvas_2d_context_clone.fill_rect(
+            player.position.x * MAP_GRID_CELL_SIZE_PX,
+            player.position.y * MAP_GRID_CELL_SIZE_PX,
+            6.0,
+            6.0,
+        );
+
+        // ray casting
+        let camera_width = game_canvas.width();
+        for ray_idx in 0..camera_width {
+            let camera_x = ((ray_idx as f64 / camera_width as f64) * 2.0) - 1.0;
+
+            // TODO: should probably implement vector methods so intentinallity is obvious (ie the
+            // scaling done to camera vector below)
+            let ray_direction = Vec2D {
+                x: (player.camera.x * camera_x) + player.direction.x,
+                y: (player.camera.y * camera_x) + player.direction.y,
+            };
+
+            // distance between 2 x and y intersects
+            let delta_x_intersect = if ray_direction.x == 0.0 {
+                f64::INFINITY
+            } else {
+                f64::abs(1.0 / ray_direction.x)
+            };
+            let delta_y_intersect = if ray_direction.y == 0.0 {
+                f64::INFINITY
+            } else {
+                f64::abs(1.0 / ray_direction.y)
+            };
+
+            let mut map_cell_position_x = player.position.x as i32;
+            let mut map_cell_position_y = player.position.y as i32;
+            let step_x: i32;
+            let step_y: i32;
+
+            let mut side_dist_x = if ray_direction.x >= 0.0 {
+                step_x = 1;
+                ((map_cell_position_x + 1) as f64 - player.position.x) * delta_x_intersect
+            } else {
+                step_x = -1;
+                (player.position.x - map_cell_position_x as f64) * delta_x_intersect
+            };
+            let mut side_dist_y = if ray_direction.y >= 0.0 {
+                step_y = -1;
+                ((map_cell_position_y + 1) as f64 - player.position.y) * delta_y_intersect
+            } else {
+                step_y = 1;
+                (player.position.y - map_cell_position_y as f64) * delta_y_intersect
+            };
+
+            let mut wall_was_hit = false;
+            let mut axis_intersected = Axis::Vertical;
+            while !wall_was_hit {
+                if side_dist_x <= side_dist_y {
+                    map_cell_position_x += step_x;
+                    side_dist_x += delta_x_intersect;
+                    axis_intersected = Axis::Vertical;
+                } else {
+                    map_cell_position_y += step_y;
+                    side_dist_y += delta_y_intersect;
+                    axis_intersected = Axis::Horizontal;
+                }
+
+                if MAP[map_cell_position_x as usize][map_cell_position_y as usize] != '0' {
+                    wall_was_hit = true;
+                }
+            }
+
+            let perpendicular_dist = match axis_intersected {
+                Axis::Vertical => side_dist_x - delta_x_intersect,
+                Axis::Horizontal => side_dist_y - delta_y_intersect,
+            };
+
+            let line_height = game_canvas.height() as f64 / perpendicular_dist;
+
+            if f64::floor(camera_width as f64 / 2.0) == ray_idx as f64 {
+                console::log_2(&"distx".into(), &side_dist_x.into());
+                console::log_2(&"disty".into(), &side_dist_y.into());
+            }
+
+            let center_screen = game_canvas.height() as f64 / 2.0;
+            let line_start = center_screen - (line_height / 2.0);
+            let line_end = line_start + line_height;
+
+            game_canvas_2d_context.begin_path();
+            let color = match MAP[map_cell_position_x as usize][map_cell_position_y as usize] {
                 '1' => "red",
                 '2' => "green",
                 '3' => "blue",
-                _ => "white",
+                _ => "black",
             };
-
-            map_canvas_2d_context.set_fill_style_str(fill_style);
-
-            let x = col_index as f64 * MAP_GRID_CELL_SIZE_PX;
-            let y = row_index as f64 * MAP_GRID_CELL_SIZE_PX;
-            map_canvas_2d_context.fill_rect(x, y, MAP_GRID_CELL_SIZE_PX, MAP_GRID_CELL_SIZE_PX);
-        }
-    }
-
-    // ### player ###
-    let player_x: u8 = 4;
-    let player_y: u8 = 2;
-    let player_dir_x: i8 = -1;
-    let player_dir_y: i8 = 0;
-    map_canvas_2d_context.set_fill_style_str("red");
-    map_canvas_2d_context.fill_rect(
-        ((player_x as f64) * MAP_GRID_CELL_SIZE_PX),
-        ((player_y as f64) * MAP_GRID_CELL_SIZE_PX),
-        6.0,
-        6.0,
-    );
-
-    let mut time = 0.0;
-    let mut prev_time = 0.0;
-
-    let mut player = Player {
-        pos_x: 4.0,
-        pos_y: 2.0,
-        dir_x: 1.0,
-        dir_y: 0.0,
-        camera_x: 0.0,
-        camera_y: 0.66,
-    };
-    map_canvas_2d_context.begin_path();
-    /*     loop { */
-    for x in 0..SCREEN_WIDTH {
-        // note for me: 2 extends range from [0, 1] to [0, 2],
-        // -1 shifts left to [-1, 1]
-        let camera_x = 2.0 * (x as f64 / SCREEN_WIDTH as f64) - 1.0;
-
-        let ray_x = player.dir_x + (player.camera_x * camera_x);
-        let ray_y = player.dir_y + (player.camera_y * camera_x);
-
-        console::log_2(&ray_x.into(), &ray_y.into());
-
-        let mut map_x = player.pos_x as i8;
-        let mut map_y = player.pos_y as i8;
-
-        let mut dist_x: f64;
-        let mut dist_y: f64;
-
-        // TODO: using unoptimized verison of this function but only having it for now in this
-        // exploratory code
-        let delta_dist_x = if ray_x == 0.0 {
-            f64::INFINITY
-        } else {
-            sqrt(f64::powi(1.0, 2) + f64::powi(ray_y / ray_x, 2)).abs()
-        };
-        let delta_dist_y = if ray_y == 0.0 {
-            f64::INFINITY
-        } else {
-            sqrt(f64::powi(1.0, 2) + f64::powi(ray_x / ray_y, 2)).abs()
-        };
-
-        let step_x: i8;
-        let step_y: i8;
-
-        let mut wall_was_hit: bool = false;
-        let mut side = Side::Vertical;
-
-        if ray_x < 0.0 {
-            step_x = -1;
-            dist_x = (player.pos_x - map_x as f64) * delta_dist_x;
-        } else {
-            step_x = 1;
-            dist_x = (map_x as f64 + 1.0 - player.pos_x) * delta_dist_x;
+            game_canvas_2d_context.set_stroke_style_str(color);
+            game_canvas_2d_context.move_to(ray_idx as f64, line_start);
+            game_canvas_2d_context.line_to(ray_idx as f64, line_end);
+            game_canvas_2d_context.stroke();
         }
 
-        if ray_y < 0.0 {
-            step_y = -1;
-            dist_y = (player.pos_y - map_y as f64) * delta_dist_y;
-        } else {
-            step_y = 1;
-            dist_y = (map_y as f64 + 1.0 - player.pos_y) * delta_dist_y;
-        }
+        window_clone
+            .borrow()
+            .request_animation_frame(f.borrow().as_ref().unwrap().as_ref().unchecked_ref())
+            .unwrap();
+    }));
 
-        // DDA alg
-        while !wall_was_hit {
-            if dist_x < dist_y {
-                dist_x += delta_dist_x;
-                map_x += step_x;
-                side = Side::Vertical;
-            } else {
-                dist_y += delta_dist_y;
-                map_y += step_y;
-                side = Side::Vertical;
-            }
-
-            if map_x >= 0 && map_y >= 0 && MAP[map_x as usize][map_y as usize] != '0' {
-                wall_was_hit = true;
-            }
-        }
-
-        let perpindicular_wall_dist = match side {
-            Side::Vertical => dist_x - delta_dist_x,
-            Side::Horizontal => dist_y - delta_dist_y,
-        };
-
-        let line_height = game_canvas.height() as f64 / perpindicular_wall_dist;
-
-        let draw_start = (-line_height / 2.0 + game_canvas.height() as f64 / 2.0).max(0.0);
-        let draw_end = (line_height / 2.0 + game_canvas.height() as f64 / 2.0)
-            .min(game_canvas.height() as f64 - 1.0);
-
-        // Choose color
-        let color = match MAP[map_x as usize][map_y as usize] {
-            'w' => "black",
-            '1' => "red",
-            '2' => "green",
-            '3' => "blue",
-            _ => "white",
-        };
-        game_canvas_2d_context.set_fill_style_str(color);
-
-        // Draw vertical line
-        game_canvas_2d_context.fill_rect(
-            x as f64,
-            draw_start as f64,
-            1.0,
-            (draw_end - draw_start) as f64,
-        );
-
-        if x % 4 == 0 {
-            map_canvas_2d_context.move_to(
-                player.pos_x * MAP_GRID_CELL_SIZE_PX,
-                player.pos_y * MAP_GRID_CELL_SIZE_PX,
-            );
-            map_canvas_2d_context.line_to(
-                player.pos_x * MAP_GRID_CELL_SIZE_PX + (ray_x * 40.0),
-                player.pos_y * MAP_GRID_CELL_SIZE_PX + (ray_y * 40.0),
-            );
-        }
-        map_canvas_2d_context.set_stroke_style_str("blue");
-    }
-    // }
-
-    map_canvas_2d_context.stroke();
-    // let heading = 0.0; //
-
-    // // game
-    // let game_canvas_element = document.get_element_by_id("game-viewport").unwrap();
-    //
-    // let game_canvas: web_sys::HtmlCanvasElement = game_canvas_element
-    //     .dyn_into::<web_sys::HtmlCanvasElement>()
-    //     .map_err(|_| ())
-    //     .unwrap();
-    //
-    // let game_canvas_width = game_canvas.width();
-    // console::log_1(&game_canvas_width.into());
-    //
-    // let game_canvas_2d_context = game_canvas
-    //     .get_context("2d")
-    //     .unwrap()
-    //     .unwrap()
-    //     .dyn_into::<web_sys::CanvasRenderingContext2d>()
-    //     .unwrap();
-    //
-    // game_canvas_2d_context.set_fill_style_str("red");
-    //
-    // game_canvas_2d_context.fill_rect(0.0, 0.0, 300.0, 300.0);
-    //
-    // loop {
-    //     console::log_1(&"sdlkjf".into());
-    // }
+    window
+        .borrow()
+        .request_animation_frame(g.borrow().as_ref().unwrap().as_ref().unchecked_ref())
+        .unwrap();
 }
